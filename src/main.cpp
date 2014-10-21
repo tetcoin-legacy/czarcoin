03185bda837f6b881f51a083ebbc317ad89885f7
Treefunder Incorporated <ceo@treefunder.com>
2014-10-21 12:21:18 -0400

    Czarcoin 0.8.6.2

--- src/main.cpp
+++ src/main.cpp
@@ -1074,7 +1074,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
 
 // static const int64 nTargetTimespan = 3.5 * 24 * 60 * 60; // Litecoin: 3.5 days
 // static const int64 nTargetSpacing = 2.5 * 60; // Litecoin: 2.5 minutes
-static const int64 nTargetTimespan = 240000000; // Czarcoin: 4 minutes
+static const int64 nTargetTimespan = 240; // Czarcoin: 4 minutes
 static const int64 nTargetSpacing = 128; // Czarcoin: 128 Seconds
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
