9d83d06485c16feb4cb28774ef3c2dfb5727f6ea
Treefunder Incorporated <ceo@treefunder.com>
2014-10-29 11:47:20 -0400

    Czarcoin Factoring

--- src/net.cpp
+++ src/net.cpp
@@ -1093,7 +1093,7 @@ void ThreadMapPort()
             }
         }
 
-        string strDesc = "Litecoin " + FormatFullVersion();
+        string strDesc = "Czarcoin " + FormatFullVersion();
 
         try {
             loop {
@@ -1173,17 +1173,17 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"litecointools.com", "dnsseed.litecointools.com"},
-    {"litecoinpool.org", "dnsseed.litecoinpool.org"},
-    {"xurious.com", "dnsseed.ltc.xurious.com"},
+    {"czarcointools.com", "dnsseed.czarcointools.com"},
+    {"czarcoinpool.org", "dnsseed.czarcoinpool.org"},
+    {"xurious.com", "dnsseed.czr.xurious.com"},
     {"koin-project.com", "dnsseed.koin-project.com"},
     {"weminemnc.com", "dnsseed.weminemnc.com"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"litecointools.com", "testnet-seed.litecointools.com"},
-    {"xurious.com", "testnet-seed.ltc.xurious.com"},
+    {"czarcointools.com", "testnet-seed.czarcointools.com"},
+    {"xurious.com", "testnet-seed.czr.xurious.com"},
     {"wemine-testnet.com", "dnsseed.wemine-testnet.com"},
     {NULL, NULL}
 };
@@ -1740,7 +1740,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. Litecoin is probably already running."), addrBind.ToString().c_str());
+            strError = strprintf(_("Unable to bind to %s on this computer. Czarcoin is probably already running."), addrBind.ToString().c_str());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString().c_str(), nErr, strerror(nErr));
         printf("%s\n", strError.c_str());
